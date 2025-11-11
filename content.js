b6aa6d49dd94b7a55d0e976dcf242fac93286605
mcskb91 <mcskb91@gmail.com>
2025-11-11 15:59:23 +0400

    feat: added mobile version

--- content.js
+++ content.js
@@ -5,6 +5,20 @@ function removeSidebars() {
   
     const rightAside = document.querySelector('aside.fixed.right-0.top-0');
     if (rightAside) rightAside.remove();
+
+    const topBanners = document.querySelectorAll('div.fixed.top-0.backdrop-blur-sm, div.fixed.top-0.border-b');
+    topBanners.forEach(banner => {
+      if (banner.querySelector('.animate-scroll')) {
+        banner.remove();
+      }
+    });
+
+    const bottomBanners = document.querySelectorAll('div.fixed.bottom-0.backdrop-blur-sm, div.fixed.bottom-0.border-t');
+    bottomBanners.forEach(banner => {
+      if (banner.querySelector('.animate-scroll')) {
+        banner.remove();
+      }
+    });
   }
   
   function observeChanges() {
